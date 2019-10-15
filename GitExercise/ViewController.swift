873a50a953758ddce183ee6d49bc94211ceaec6b
Prabhat <prabhat@vinsol.com>
2019-10-15 11:37:45 +0530

    This is my fifth commit with showing two heads

--- GitExercise/ViewController.swift
+++ GitExercise/ViewController.swift
@@ -11,10 +11,6 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         //Do any work after view appear
     }
-    
-    private func showMessage() {
-        print("Hello World!")
-    }
 
 }
 

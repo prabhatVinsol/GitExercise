d103fd4f8570a682b80ecc71a8971ccc61068869
Prabhat <prabhat@vinsol.com>
2019-10-15 13:54:04 +0530

    This first commit on branch feature1

--- GitExercise/ViewController.swift
+++ GitExercise/ViewController.swift
@@ -19,5 +19,9 @@ class ViewController: UIViewController {
     private func secondPrintMessage() {
         print("Hello")
     }
+    
+    private func methodAdddedForFeature1() {
+        print("Branch feature1")
+    }
 }
 
